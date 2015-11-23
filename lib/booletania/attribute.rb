3dc1107accbb577ad02e5eff40bdd64ee7c394ea
ryoff <ryoffes@gmail.com>
2015-11-23 23:32:40 +0900

    refactoring

--- lib/booletania/attribute.rb
+++ lib/booletania/attribute.rb
@@ -1,11 +1,11 @@
 module Booletania
   class Attribute
-    def self.define_methods!(mod, boolean_columns)
+    def self.define_methods!(klass, boolean_columns)
       boolean_columns.each do |boolean_column|
-        mod.module_eval <<-RUBY, __FILE__, __LINE__ + 1
+        klass.class_eval <<-RUBY, __FILE__, __LINE__ + 1
           def #{boolean_column.name}_text
             # http://yaml.org/type/bool.html
-            I18n.t "booletania.#{mod.name.underscore}.#{boolean_column.name}." + #{boolean_column.name}.__send__(:to_s), default: #{boolean_column.name}.__send__(:to_s)
+            I18n.t "booletania.#{klass.name.underscore}.#{boolean_column.name}." + #{boolean_column.name}.__send__(:to_s), default: #{boolean_column.name}.__send__(:to_s)
           end
         RUBY
       end
