9fa718922fbd38fa50e63773ecbf1efb40154849
ryoff <ryoffes@gmail.com>
2016-02-28 01:59:00 +0900

    cosme

--- lib/booletania/attribute.rb
+++ lib/booletania/attribute.rb
@@ -3,13 +3,13 @@ module Booletania
     class << self
       def define_methods!(klass, boolean_columns)
         boolean_columns.each do |boolean_column|
-          define_attributes_text(klass, boolean_column)
+          define_attribute_text(klass, boolean_column)
         end
       end
 
       private
 
-      def define_attributes_text(klass, boolean_column)
+      def define_attribute_text(klass, boolean_column)
         klass.class_eval <<-RUBY, __FILE__, __LINE__ + 1
           def #{boolean_column.name}_text
             # http://yaml.org/type/bool.html
