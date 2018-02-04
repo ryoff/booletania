36f72447eb84712e49b4358372123dfed6b32499
ryoff <ryoffes@gmail.com>
2018-02-04 14:21:29 +0900

    Enable :activerecord i18n key namespace

--- spec/booletania_spec.rb
+++ spec/booletania_spec.rb
@@ -25,41 +25,95 @@ describe Booletania do
   end
 
   describe "#_text" do
-    let!(:invitation) { Invitation.create(accepted1: accepted1) }
+    let!(:invitation) { Invitation.create(accepted1: accepted1, accepted2: accepted2, accepted3: accepted3) }
+    let(:accepted1) { true }
+    let(:accepted2) { true }
+    let(:accepted3) { true }
     after { Invitation.delete_all }
 
-    subject { invitation.accepted1_text }
+    shared_examples_for 'translated by :booletania i18n key' do
+      context "lang is ja" do
+        before { I18n.locale = :ja }
 
-    context "lang is ja" do
-      before { I18n.locale = :ja }
+        context "column is true" do
+          let(:accepted) { true }
+          it { is_expected.to eq '承諾' }
+        end
 
-      context "column is true" do
-        let(:accepted1) { true }
-
-        it { is_expected.to eq '承諾' }
+        context "column is false" do
+          let(:accepted) { false }
+          it { is_expected.to eq '拒否' }
+        end
       end
 
-      context "column is false" do
-        let(:accepted1) { false }
+      context "lang is en" do
+        before { I18n.locale = :en }
 
-        it { is_expected.to eq '拒否' }
+        context "column is true" do
+          let(:accepted) { true }
+          it { is_expected.to eq 'accept' }
+        end
+
+        context "column is false" do
+          let(:accepted) { false }
+          it { is_expected.to eq 'deny' }
+        end
+      end
+
+      context "lans is invalid" do
+        before { I18n.locale = :xx }
+        let(:accepted) { true }
+
+        it { is_expected.to eq 'True' }
       end
     end
 
-    context "lang is en" do
-      before { I18n.locale = :en }
+    shared_examples_for 'translated by :activerecord i18n key' do
+      context "lang is ja" do
+        before { I18n.locale = :ja }
 
-      context "column is true" do
-        let(:accepted1) { true }
+        context "column is true" do
+          let(:accepted) { true }
+          it { is_expected.to eq '許可' }
+        end
 
-        it { is_expected.to eq 'accept' }
+        context "column is false" do
+          let(:accepted) { false }
+          it { is_expected.to eq '不可' }
+        end
       end
 
-      context "column is false" do
-        let(:accepted1) { false }
+      context "lang is en" do
+        before { I18n.locale = :en }
 
-        it { is_expected.to eq 'deny' }
+        context "column is true" do
+          let(:accepted) { true }
+          it { is_expected.to eq 'ok' }
+        end
+
+        context "column is false" do
+          let(:accepted) { false }
+          it { is_expected.to eq 'ng' }
+        end
       end
+    end
+
+    describe "#accepted1_text. only has :booletania i18n key" do
+      subject { invitation.accepted1_text }
+      let(:accepted1) { accepted }
+      it_behaves_like 'translated by :booletania i18n key'
+    end
+
+    describe "#accepted2_text. has :booletania and :activerecord i18n key" do
+      subject { invitation.accepted2_text }
+      let(:accepted2) { accepted }
+      it_behaves_like 'translated by :booletania i18n key'
+    end
+
+    describe "#accepted3_text. has only :activerecord i18n key" do
+      subject { invitation.accepted3_text }
+      let(:accepted3) { accepted }
+      it_behaves_like 'translated by :activerecord i18n key'
     end
   end
 
